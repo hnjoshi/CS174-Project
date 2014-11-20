3ed79a5bcf3c95fc958a3f16da8a09357be13baa
Jeffrey Su <sogajeffrey@gmail.com>
2014-11-20 11:02:41 -0800

    updated dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -1,13 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "youthcyb_su";
-$password = "007680959";
-$dbname = "youthcyb_su";
+$username = "root";
+$password = "secret123";
+$dbname = "174project";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 ?>
